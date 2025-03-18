5d440b7246e03bf95e5740a083433622f88a40cf
nottsatsa <tsatsralsainbolor@gmail.com>
2025-03-18 12:40:15 +0800

    navbar, poster slide boslon

--- movie/src/app/page.tsx
+++ movie/src/app/page.tsx
@@ -1,6 +1,90 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { BsFilm } from "react-icons/bs";
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { MdOutlineDarkMode } from "react-icons/md";
+
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return <Button />;
+  return (
+    <>
+      <nav className="flex items-center justify-around p-4">
+        <div className="flex gap-2 items-center">
+          <BsFilm color="#4338CA" />
+          <h6 className="text-indigo-700 font-[700] italic">Movie Z</h6>
+        </div>
+
+        <div className="flex gap-3">
+          <NavigationMenu className="border border-[#E4E4E7] rounded-md">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+
+          <div className="flex items-center gap-2.5 h-fit border border-[#E4E4E7] pr-3 pl-3 rounded-lg">
+            <HiMagnifyingGlass />
+            <Input className="border-0" />
+          </div>
+        </div>
+
+        <Button variant="outline">
+          <MdOutlineDarkMode />
+        </Button>
+      </nav>
+      <div>
+        <Carousel className="relative">
+          <CarouselContent>
+            <CarouselItem className="flex items-center w-full h-200 overflow-hidden">
+              <img
+                className="justify-center w-full"
+                src="https://s3-alpha-sig.figma.com/img/c78e/5e57/16d36abbdaa8df480db189d5729e384a?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mLG2xOJZNT2vGCrvhDwo3-iKl0QLHOJCxUbykqD81OWYT61RDmWH~sY5qc4qVMmDHRoLdT3VXAnqpxjl4QRY7lvqwtvVTj2-RScRPADrSRE2X1dKJ6MNwI89GQsAr7CVA~Sw886s4cN3GzZCxbhX6nG5wCcsdExQ3ZifH-DrPK1y2qNpWDmJzamRmYUQB4G5gKUvdNeqjPEES5nuyWmp4tVWbJDWV1Ve6DECdtwn6WwE~0puD445Fe7qQpsvTO15bYmHP3E7sN6ZamI~BBe1H7Aisb1JjhHE35MH~r0CHClF6Ayy8aDTsnbuKYmE-rzkB3IlXZLoaZaJNRHFDc~Erg__"
+                alt="wicked"
+              />
+            </CarouselItem>
+            <CarouselItem className="flex items-center w-full h-200 overflow-hidden">
+              <img
+                className="w-full"
+                src="https://s3-alpha-sig.figma.com/img/c78e/5e57/16d36abbdaa8df480db189d5729e384a?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mLG2xOJZNT2vGCrvhDwo3-iKl0QLHOJCxUbykqD81OWYT61RDmWH~sY5qc4qVMmDHRoLdT3VXAnqpxjl4QRY7lvqwtvVTj2-RScRPADrSRE2X1dKJ6MNwI89GQsAr7CVA~Sw886s4cN3GzZCxbhX6nG5wCcsdExQ3ZifH-DrPK1y2qNpWDmJzamRmYUQB4G5gKUvdNeqjPEES5nuyWmp4tVWbJDWV1Ve6DECdtwn6WwE~0puD445Fe7qQpsvTO15bYmHP3E7sN6ZamI~BBe1H7Aisb1JjhHE35MH~r0CHClF6Ayy8aDTsnbuKYmE-rzkB3IlXZLoaZaJNRHFDc~Erg__"
+                alt="wicked"
+              />
+            </CarouselItem>
+            <CarouselItem className="flex items-center w-full h-200 overflow-hidden">
+              {" "}
+              <img
+                className="w-full"
+                src="https://s3-alpha-sig.figma.com/img/c78e/5e57/16d36abbdaa8df480db189d5729e384a?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=mLG2xOJZNT2vGCrvhDwo3-iKl0QLHOJCxUbykqD81OWYT61RDmWH~sY5qc4qVMmDHRoLdT3VXAnqpxjl4QRY7lvqwtvVTj2-RScRPADrSRE2X1dKJ6MNwI89GQsAr7CVA~Sw886s4cN3GzZCxbhX6nG5wCcsdExQ3ZifH-DrPK1y2qNpWDmJzamRmYUQB4G5gKUvdNeqjPEES5nuyWmp4tVWbJDWV1Ve6DECdtwn6WwE~0puD445Fe7qQpsvTO15bYmHP3E7sN6ZamI~BBe1H7Aisb1JjhHE35MH~r0CHClF6Ayy8aDTsnbuKYmE-rzkB3IlXZLoaZaJNRHFDc~Erg__"
+                alt="wicked"
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-10 top-[50%]" />
+          <CarouselNext className="absolute right-10 top-[50%]" />
+        </Carousel>
+      </div>
+      <main></main>
+    </>
+  );
 }
---
+++ movie/src/components/Movie.tsx
@@ -0,0 +1,3 @@
+export const Movie = ({ poster, rating, movieName }) => {
+  return <div className="w-57.5 h-110 rounded-lg"></div>;
+};
